f69f86af57f8c9998bad6e94e998e8105ceaef46
umari <umarijaz3014@gmail.com>
2020-10-16 17:45:24 -0700

    created empty function declaration for showPage function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,10 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
 
+
+}
 
 
 /*
