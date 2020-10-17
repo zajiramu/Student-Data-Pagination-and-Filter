7f23235396e1aba57757cb96458d2ea6523306e8
umari <umarijaz3014@gmail.com>
2020-10-16 22:08:25 -0700

    created function calcItemsPerPage and its call inside showPage
    function calcItemsPerPage uses the list length and page number as parameters to compute the number of student data items that go on a single page.

--- js/script.js
+++ js/script.js
@@ -18,7 +18,12 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(list, page) {
+   let itemsPerPage = calcItemsPerPage(list.length, page);
+   
 
+}
+
+function calcItemsPerPage(listLength, page) {
 
 }
 
