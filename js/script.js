ca6faccfc7edbfa20da242a0dac9305c5739cda5
umari <umarijaz3014@gmail.com>
2020-10-17 22:34:51 -0700

    updated script.js
    added function calls for functions showPage & addPagination
    added event listener for ul element containing pagination buttons

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ let itemsPerPage = 9;
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+
 function showPage(list, page) {
    let startIndex = (page * itemsPerPage) - itemsPerPage;
    let endIndex   = page * itemsPerPage;
@@ -39,24 +40,22 @@ function showPage(list, page) {
          listUL.insertAdjacentHTML('beforeend', li);
       }
    }
-
 }
 
 
-
+// gets the ul element with the class link-list
+   // stores it in pageButtons
+let pageButtons = document.getElementsByClassName('link-list')[0]; 
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+Creates the `addPagination` function
+creates and inserts/appends the elements needed for the pagination buttons
 */
+
 function addPagination(list) {
    // calcs number of pages needed based on length of list array argument
    // stores result in numPages
    let numPages = Math.ceil(list.length / 9);
-
-   // gets the ul element with the class link-list
-   // stores it in pageButtons
-   let pageButtons = document.getElementsByClassName('student-list')[0]; 
 
    // loops over the number of pages 
    for(let i = 1; i <= numPages; i++) {
@@ -68,9 +67,44 @@ function addPagination(list) {
                </li>`; 
       // inserts li HTML element created above to the
       // ul element stored in pageButtons 
-      pageButtons.insertAdjacentHTML('beforeend',li);
+      pageButtons.insertAdjacentHTML('beforeend', li);
+
+      // on first iteration, or creation of the first button
+      // set the first button's class to 'active'
+      if(i == 1) {
+         pageButtons.firstElementChild.className = 'active';
+         console.log('it works');
+      }
    }
 }
 
+// Call functions showPage & addPagination 
+// to add pagination buttons and display first nine
+// students onto the page
+showPage(data, 1);
+addPagination(data);
 
-// Call functions
+// adds event listener to ul element inside pageButtons
+// to listen for click events on the buttons
+pageButtons.addEventListener('click', (e) => {
+   // checks if target is button
+   if(e.target.tagName === 'BUTTON') {
+      // console.log('it works');
+      // gets button element clicked & stores it in button 
+      let button = e.target;
+      // sets activeLi to the list element containing the 'active' class
+      let activeLi = pageButtons.getElementsByClassName('active')[0];
+      // sets the className of the list element in activeLi to an empty string
+      activeLi.className = '';
+      // sets buttonLi to the li parent element of the button that was clicked
+      let buttonLi = button.parentElement;
+      // gives li parent of clicked button the 'active' class
+      buttonLi.className = 'active';
+      // stores page # of on the button by getting the text contents of button
+      let pageNum = button.textContent;
+      // calls function showPage with array of student data objects & the page #
+      // this function will display a new set of students on the page
+      showPage(data, pageNum);
+   }
+});
+
