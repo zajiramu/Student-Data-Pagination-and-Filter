3f68865cfbc82f374ba89448b8fbfc5ad7cbf4c5
umari <umarijaz3014@gmail.com>
2020-10-17 17:05:49 -0700

    updated script.js
    completed definition of function addPagination

--- js/script.js
+++ js/script.js
@@ -50,7 +50,26 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination(list) {
+   // calcs number of pages needed based on length of list array argument
+   // stores result in numPages
+   let numPages = Math.ceil(list.length / 9);
 
+   // gets the ul element with the class link-list
+   // stores it in pageButtons
+   let pageButtons = document.getElementsByClassName('student-list')[0]; 
+
+   // loops over the number of pages 
+   for(let i = 1; i <= numPages; i++) {
+      // creates html list element as a template literal
+      // with a button element as a child
+      // and stores the result in li
+      let li = `<li>
+                  <button type="button">${i}</button>
+               </li>`; 
+      // inserts li HTML element created above to the
+      // ul element stored in pageButtons 
+      pageButtons.insertAdjacentHTML('beforeend',li);
+   }
 }
 
 
